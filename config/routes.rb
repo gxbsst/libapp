0cea9e456a2bec9cb96c1c3a551f9f67fde3bc24
gxbsst <gxbsst@gmail.com>
2013-06-05 17:37:15 +0800

    SessionController new action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Libapp::Application.routes.draw do
   get '/register', to: "readers#new", as: "register"
   resources :readers
   root to: 'home#index'
-  get '/login', to: "sessions#new", as: 'login'
+  get '/login', to: "sessions#new", as: 'sessions'
 end

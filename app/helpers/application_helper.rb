6904d8788dc3a315ef62e229b8f63c1a83003926
gxbsst <gxbsst@gmail.com>
2013-06-06 12:34:54 +0800

    Add helper rspec test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def top_menu
+    if current_user
+     "Welcome, #{current_user.email}"
+    else
+      content_tag(:div, id: "top_menu") do
+        link_to "Register", register_path
+      end
+    end
+  end
 end
